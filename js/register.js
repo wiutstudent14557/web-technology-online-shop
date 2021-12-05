922ddf75978ec988f4f05670fe696416e05ce58c
wiutstudent14557 <14557student@gmail.com>
2021-12-05 21:30:29 +0500

    authentication succeed

--- js/register.js
+++ js/register.js
@@ -119,7 +119,7 @@ loginForm.addEventListener("submit", (e)=>{
         if(localStorage.getItem('users') == null){
            alert("sorry you are not authorized")
         }else if(JSON.parse(localStorage.getItem('users'))[0].email === loginEmail.value && JSON.parse(localStorage.getItem('users'))[0].password === loginPassword.value){
-            window.location.href = "../web-pages/main.html"
+            document.getElementById("hiddenBtn").click()
         }else{
             alert("sorry you are not authorized")
         }
